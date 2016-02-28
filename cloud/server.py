c4edd325873ab63aeed577f39e3384b48825d109
dinodigma <leonardinodigma@berkeley.edu>
2016-02-28 03:55:44 -0800

    fixed plots

--- cloud/server.py
+++ cloud/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import instructions
 
 # Handling Data
@@ -152,29 +152,14 @@ def data():
     return jsonify(result={"status": 200})
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    ''' 
-
+@app.route('/get_image')
+def get_image():
+    # if request.args.get('type') == '1':
+    #    filename = 'ok.gif'
+    # else:
+    #    filename = 'error.gif'
+    plot_prog()
+    return send_file('ex_progress.jpg', mimetype='image/jpg')
 
 
 if __name__ == '__main__':
